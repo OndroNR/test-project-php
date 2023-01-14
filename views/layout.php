e1b144c0c99ca162193c6fa7deff016f64a4ceed
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2023-01-14 12:31:34 +0100

    Replace jquery and boostrap with current versions from CDN

--- views/layout.php
+++ views/layout.php
@@ -6,14 +6,10 @@
     <title>PHP Test Application</title>
     
 	<link href="favicon.ico" type="image/x-icon" rel="icon" />
-	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />	
-    
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/application.css">
-    
-    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
+	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
 
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/application.css">
   </head>
   <body>
 
@@ -21,7 +17,9 @@
     
     <?= $content ?>
     
-  </div>  
+  </div>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.3.slim.js" integrity="sha256-DKU1CmJ8kBuEwumaLuh9Tl/6ZB6jzGOBV/5YpNE2BWc=" crossorigin="anonymous"></script>
   </body>
 </html>
